cc3b2405c23005a88fec048382014d3529dfa6e6
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-08-21 10:04:18 -0700

    changed file name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRouter");
+const apiRoutes = require("./routes/apiRouter");
 //Initialize the app and setups a port for the same 
 const app = express();
 const PORT = process.env.PORT || 3001;
